ae8d9f1c87b1bbaec05dc825167db04d8f65f547
StevenGG19 <stevengg39@gmail.com>
2021-10-23 21:54:01 -0500

    Base project

---
+++ app/src/main/java/com/steven/foody/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.steven.foody
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.steven.foody.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.btnNavigation.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/steven/foody/RecipesFragment.kt
@@ -0,0 +1,23 @@
+package com.steven.foody
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.steven.foody.databinding.FragmentRecipesBinding
+
+class RecipesFragment : Fragment() {
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        binding.recyclerView.showShimmer()
+        return binding.root
+    }
+
+}
